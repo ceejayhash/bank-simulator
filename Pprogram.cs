80cd52e3d8b000a95dbe861c46a428a13c667b6b
ceejayhash <cheenacarongay@gmail.com>
2026-02-14 11:01:18 +0800

    Update Pprogram.cs

--- Pprogram.cs
+++ Pprogram.cs
@@ -1,1 +1,38 @@
+namespace ATMApp.Services
+{
+    public class BankingServices
+    {
+        private decimal balance = 1000.00m;
+        private decimal lastTransaction = 0;
 
+        public decimal Balance()
+        {
+            return balance;
+        }
+
+        public bool deposit(decimal amount)
+        {
+            if (amount <= 0)
+                return false;
+
+            balance += amount;
+            lastTransaction = amount;
+            return true;
+        }
+
+        public bool withdraw(decimal amount)
+        {
+            if (amount <= 0 || amount > balance)
+                return false;
+
+            balance -= amount;
+            lastTransaction = amount;
+            return true;
+        }
+
+        public decimal GetLastTransaction()
+        {
+            return lastTransaction;
+        }
+    }
+}
