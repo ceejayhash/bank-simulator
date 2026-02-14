42ce4142e02235b3bb7dec425ad6a9003953ed03
ceejayhash <cheenacarongay@gmail.com>
2026-02-14 11:02:17 +0800

    Update BankingServices.cs

--- BankingServices.cs
+++ BankingServices.cs
@@ -1,1 +1,38 @@
+public class BankingService
+{
+    private decimal lastTransactionAmount = 0;
+
+    public decimal GetBalance(decimal balance)
+    {
+        return balance;
+    }
+
+    public bool Deposit(ref decimal balance, decimal amount)
+    {
+        if (amount <= 0)
+            return false;
+
+        balance += amount;
+        lastTransactionAmount = amount;
+        return true;
+    }
+
+    public void Withdraw(ref decimal balance, decimal amount, out bool success)
+    {
+        if (amount <= 0 || amount > balance)
+        {
+            success = false;
+            return;
+        }
+
+        balance -= amount;
+        lastTransactionAmount = amount;
+        success = true;
+    }
+
+    public decimal GetLastTransaction()
+    {
+        return lastTransactionAmount;
+    }
+}
 
