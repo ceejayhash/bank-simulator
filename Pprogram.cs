891d0c73b00a0d01306f70153af170de9a2075f5
ceejayhash <cheenacarongay@gmail.com>
2026-02-14 11:02:55 +0800

    Update Pprogram.cs

--- Pprogram.cs
+++ Pprogram.cs
@@ -1,38 +1,88 @@
-namespace ATMApp.Services
+using System;
+
+class Program
 {
-    public class BankingServices
+    static void Main()
     {
-        private decimal balance = 1000.00m;
-        private decimal lastTransaction = 0;
+        decimal balance = 1000.00m;
+        BankingService service = new BankingService();
+        BankingView view = new BankingView();
 
-        public decimal Balance()
+        view.ShowHeader("Your Name Here");
+        view.ShowInitialBalance(balance);
+
+        while (true)
         {
-            return balance;
-        }
+            view.ShowMenu();
+            int choice;
 
-        public bool deposit(decimal amount)
-        {
-            if (amount <= 0)
-                return false;
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                view.ShowInvalidOption();
+                continue;
+            }
 
-            balance += amount;
-            lastTransaction = amount;
-            return true;
-        }
+            switch (choice)
+            {
+                case 1:
+                    view.DisplayBalance(service.GetBalance(balance));
+                    break;
 
-        public bool withdraw(decimal amount)
-        {
-            if (amount <= 0 || amount > balance)
-                return false;
+                case 2:
+                    Console.Write("Enter amount to deposit: ");
+                    decimal depositAmount;
+                    if (!decimal.TryParse(Console.ReadLine(), out depositAmount))
+                    {
+                        view.ShowInvalidAmount("deposit");
+                        continue;
+                    }
 
-            balance -= amount;
-            lastTransaction = amount;
-            return true;
-        }
+                    if (!service.Deposit(ref balance, depositAmount))
+                    {
+                        view.ShowInvalidAmount("deposit");
+                        continue;
+                    }
 
-        public decimal GetLastTransaction()
-        {
-            return lastTransaction;
+                    view.ShowDepositSuccess(balance);
+                    break;
+
+                case 3:
+                    Console.Write("Enter amount to withdraw: ");
+                    decimal withdrawAmount;
+                    if (!decimal.TryParse(Console.ReadLine(), out withdrawAmount))
+                    {
+                        view.ShowInvalidAmount("withdrawal");
+                        continue;
+                    }
+
+                    bool success;
+                    service.Withdraw(ref balance, withdrawAmount, out success);
+
+                    if (!success)
+                    {
+                        if (withdrawAmount <= 0)
+                            view.ShowInvalidAmount("withdrawal");
+                        else
+                            view.ShowInsufficientBalance();
+
+                        continue;
+                    }
+
+                    view.ShowWithdrawSuccess(balance);
+                    break;
+
+                case 4:
+                    view.ShowMiniStatement(balance, service.GetLastTransaction());
+                    break;
+
+                case 5:
+                    view.ShowExitMessage();
+                    return;
+
+                default:
+                    view.ShowInvalidOption();
+                    break;
+            }
         }
     }
 }
